d0dce662064730fcda8a8884b1a9fc831f5f2152
SameerGadge <42699060+SameerGadge@users.noreply.github.com>
2026-03-16 14:58:08 +0530

    Load and transform MNIST dataset
    Added MNIST dataset loading and transformation.

--- classification.py
+++ classification.py
@@ -1,1 +1,18 @@
+import torchvision
+import PIL
+from IPython.display import display
+from torchvision import transforms
+from torchvision.datasets import mnist
 
+train_dataset = mnist.MNIST(root='./data', train=True, download=True)
+test_dataset = mnist.MNIST(root='./data', train=False, download=True)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+train_dataset.transform = transform
+test_dataset.transform = transform
+
+x_transformed = train_dataset[0]
